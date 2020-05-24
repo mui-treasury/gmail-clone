f8515b723a79354b25859537a4a575094ad803fa
siriwatknp <siriwatkunaporn@gmail.com>
2020-05-24 12:42:26 +0700

    feat: 🎸 add Icon to MailListItem

--- src/components/AppContent.tsx
+++ src/components/AppContent.tsx
@@ -15,9 +15,15 @@ import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
 import Settings from "@material-ui/icons/Settings";
 import Keyboard from "@material-ui/icons/Keyboard";
 import Edit from "@material-ui/icons/Edit";
+import Inbox from '@material-ui/icons/Inbox';
+import LocalOffer from '@material-ui/icons/LocalOffer';
+import People from '@material-ui/icons/People';
+import Info from '@material-ui/icons/Info';
 import { useSizedIconButtonStyles } from "@mui-treasury/styles/iconButton/sized";
 import { useRowGutterStyles } from "@mui-treasury/styles/gutter/row";
 import ArrowMenu from "@mui-treasury/components/menu/arrow";
+import { GmailTabs, GmailTabItem } from "@mui-treasury/components/tabs/gmail";
+import MailListItem from './MailListItem'
 
 const Div = styled("div")`
   height: 48px;
@@ -46,64 +52,88 @@ const AppContent = () => {
   const gutterStyles = useRowGutterStyles({ size: "0.25rem" });
   const menuItemClasses = useStyles();
   const checkboxClasses = useCheckboxStyles();
+  const [index, setIndex] = React.useState(0);
   return (
-    <Div>
-      <Box display="inline-flex" className={gutterStyles.parent}>
-        <ArrowMenu
-          renderElement={({ styles, onClose }) => (
-            <Checkbox
-              classes={checkboxClasses}
-              className={styles.button}
-              color={"default"}
-              onChange={onClose}
-            />
-          )}
-        >
-          <MenuItem classes={menuItemClasses}>All</MenuItem>
-          <MenuItem classes={menuItemClasses}>None</MenuItem>
-          <MenuItem classes={menuItemClasses}>Read</MenuItem>
-          <MenuItem classes={menuItemClasses}>Unread</MenuItem>
-        </ArrowMenu>
-        <IconButton classes={actionStyles}>
-          <Refresh />
-        </IconButton>
-        <IconButton classes={actionStyles}>
-          <MoreVert />
-        </IconButton>
-      </Box>
-      <Box display='inline-flex' alignItems="center" ml="auto" className={gutterStyles.parent}>
-        <Box fontSize={12} color="text.secondary">
-          1-50 of 1,971
+    <>
+      <Div>
+        <Box display="inline-flex" className={gutterStyles.parent}>
+          <ArrowMenu
+            renderElement={({ styles, onClose }) => (
+              <Checkbox
+                classes={checkboxClasses}
+                className={styles.button}
+                color={"default"}
+                onChange={onClose}
+              />
+            )}
+          >
+            <MenuItem classes={menuItemClasses}>All</MenuItem>
+            <MenuItem classes={menuItemClasses}>None</MenuItem>
+            <MenuItem classes={menuItemClasses}>Read</MenuItem>
+            <MenuItem classes={menuItemClasses}>Unread</MenuItem>
+          </ArrowMenu>
+          <IconButton classes={actionStyles}>
+            <Refresh />
+          </IconButton>
+          <IconButton classes={actionStyles}>
+            <MoreVert />
+          </IconButton>
         </Box>
-        <IconButton disabled classes={actionStyles}>
-          <KeyboardArrowLeft />
-        </IconButton>
-        <IconButton classes={actionStyles}>
-          <KeyboardArrowRight />
-        </IconButton>
-        <ArrowMenu
-          renderElement={({ styles, onClose }) => (
-            <IconButton
-              className={styles.button}
-              color={"default"}
-              onChange={onClose}
-            >
-              <Keyboard />
-            </IconButton>
-          )}
+        <Box
+          display="inline-flex"
+          alignItems="center"
+          ml="auto"
+          className={gutterStyles.parent}
         >
-          <MenuItem classes={menuItemClasses}>
-            <ListItemIcon style={{ minWidth: 32 }}>
-              <Edit fontSize={"small"} />
-            </ListItemIcon>
-            English
-          </MenuItem>
-        </ArrowMenu>
-        <IconButton classes={actionStyles}>
-          <Settings />
-        </IconButton>
-      </Box>
-    </Div>
+          <Box fontSize={12} color="text.secondary">
+            1-50 of 1,971
+          </Box>
+          <IconButton disabled classes={actionStyles}>
+            <KeyboardArrowLeft />
+          </IconButton>
+          <IconButton classes={actionStyles}>
+            <KeyboardArrowRight />
+          </IconButton>
+          <ArrowMenu
+            renderElement={({ styles, onClose }) => (
+              <IconButton
+                className={styles.button}
+                color={"default"}
+                onChange={onClose}
+              >
+                <Keyboard />
+              </IconButton>
+            )}
+          >
+            <MenuItem classes={menuItemClasses}>
+              <ListItemIcon style={{ minWidth: 32 }}>
+                <Edit fontSize={"small"} />
+              </ListItemIcon>
+              English
+            </MenuItem>
+          </ArrowMenu>
+          <IconButton classes={actionStyles}>
+            <Settings />
+          </IconButton>
+        </Box>
+      </Div>
+      <GmailTabs value={index} onChange={(_, value) => setIndex(value)}>
+        <GmailTabItem icon={<Inbox />} label={"Primary"} />
+        <GmailTabItem
+          icon={<People />}
+          label={"Social"}
+          tag={"2 new"}
+          subLabel={"Youtube, LinkedIn"}
+        />
+        <GmailTabItem
+          icon={<LocalOffer />}
+          label={"Promotions"}
+          subLabel={"Pattern Matching, Medium Daily"}
+        />
+        <GmailTabItem icon={<Info />} label={"Updates"} tag={"15 new"} />
+      </GmailTabs>
+      <MailListItem />
+    </>
   );
 };
 
--- src/components/MailListItem.tsx
+++ src/components/MailListItem.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import cx from "clsx";
 import styled from "styled-components";
-import { IconButton, Box, Tooltip, withStyles } from "@material-ui/core";
+import { IconButton } from "@material-ui/core";
 import Checkbox from "@material-ui/icons/CheckBox";
 import CheckBoxOutlineBlank from "@material-ui/icons/CheckBoxOutlineBlank";
 import Star from "@material-ui/icons/Star";
@@ -15,48 +14,25 @@ import WatchLater from "@material-ui/icons/WatchLater";
 import Label from "@material-ui/icons/Label";
 import { useSizedIconButtonStyles } from "@mui-treasury/styles/iconButton/sized";
 import { useRowGutterStyles } from "@mui-treasury/styles/gutter/row";
-
-const StyledTooltip = withStyles({
-  tooltip: {
-    backgroundColor: "rgba(0,0,0,0.72)",
-    fontSize: 12,
-    marginTop: 0,
-  },
-})(Tooltip);
-
-const GmailAction = styled(IconButton)`
-  color: rgba(0, 0, 0, 0.54);
-  &:hover {
-    color: #000;
-  }
-`;
+import { useGrabIconStyles } from '@mui-treasury/styles/icon/grab';
 
 const StyledDrag = styled(DragIndicator)`
-  color: rgba(0, 0, 0, 0.2);
+  color: rgba(0,0,0,0.2);
   align-self: center;
-  cursor: grab;
-  &:active {
-    cursor: grabbing;
-  }
-`;
+`
 
 const Div = styled("div")`
   height: 40px;
   display: flex;
   align-items: center;
   box-shadow: inset 0 -1px 0 0 rgba(100, 121, 143, 0.122);
-  &.MailListItem-read {
-    background-color: rgba(242, 245, 245, 0.8);
-  }
   &:hover {
     box-shadow: inset 1px 0 0 #dadce0, inset -1px 0 0 #dadce0,
       0 1px 2px 0 rgba(60, 64, 67, 0.3), 0 1px 3px 1px rgba(60, 64, 67, 0.15);
+    z-index: 1;
     ${StyledDrag} {
       opacity: 1;
     }
-  }
-  & > .MailListItem-parent {
-    display: inline-flex;
   }
   ${StyledDrag} {
     opacity: 0;
@@ -67,134 +43,28 @@ const Div = styled("div")`
 const StyledIconButton = styled(IconButton)`
   color: rgba(0, 0, 0, 0.2);
   &:hover {
-    color: rgba(0, 0, 0, 0.6);
-  }
-  &.MailListItem-checked {
-    color: rgba(0, 0, 0, 0.87);
-  }
-  &.MailListItem-starred,
-  &.MailListItem-labeled {
-    color: #f8cb69;
-  }
-`;
-
-const Text = styled("div")`
-  font-size: 14px;
-  -webkit-font-smoothing: antialiased;
-  font-family: Roboto, RobotoDraft, Helvetica, Arial, sans-serif;
-  color: #5f6368;
-  min-width: 0;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-
-  b {
     color: rgba(0, 0, 0, 0.87);
   }
 `;
 
-const Title = styled(Text)`
-  flex-basis: 200px;
-  max-width: 200px;
-  flex-shrink: 0;
-
-  & > *:not(:first-child) {
-    margin-left: 4px;
-    font-size: 12px;
-  }
-`;
-
-const Date = styled(Text)`
-  font-size: 12px;
-  flex-basis: 100px;
-  flex-shrink: 0;
-  text-align: right;
-  padding-right: 16px;
-`;
-
-interface MailListItemProps {
-  title?: React.ReactNode;
-  description?: React.ReactNode;
-  read?: boolean;
-  initialChecked?: boolean;
-  initialStarred?: boolean;
-  initialLabeled?: boolean;
-}
-
-const MailListItem = ({
-  title,
-  description,
-  read = false,
-  initialChecked = false,
-  initialStarred = false,
-  initialLabeled = false,
-}: MailListItemProps) => {
+const MailListItem = () => {
   const actionStyles = useSizedIconButtonStyles({ childSize: 20, padding: 10 });
   const gutterStyles = useRowGutterStyles({ size: -10, before: -8 });
-  const [checked, setChecked] = React.useState(initialChecked);
-  const [starred, setStarred] = React.useState(initialStarred);
-  const [labeled, setLabeled] = React.useState(initialLabeled);
-  const [hovered, setHovered] = React.useState(false);
+  const grabStyles = useGrabIconStyles()
   return (
-    <Div
-      className={cx(read && "MailListItem-read")}
-      onMouseOver={() => setHovered(true)}
-      onMouseLeave={() => setHovered(false)}
-    >
-      <StyledDrag fontSize="small" />
-      <div className={cx("MailListItem-parent", gutterStyles.parent)}>
-        <StyledIconButton
-          className={cx(checked && "MailListItem-checked")}
-          classes={actionStyles}
-          onClick={() => setChecked(!checked)}
-        >
-          {checked ? <Checkbox /> : <CheckBoxOutlineBlank />}
+    <Div>
+      <StyledDrag className={grabStyles.root} />
+      <div className={gutterStyles.parent}>
+        <StyledIconButton classes={actionStyles}>
+          <CheckBoxOutlineBlank />
         </StyledIconButton>
-        <StyledIconButton
-          className={cx(starred && "MailListItem-starred")}
-          classes={actionStyles}
-          onClick={() => setStarred(!starred)}
-        >
-          {starred ? <Star /> : <StarBorder />}
+        <StyledIconButton classes={actionStyles}>
+          <StarBorder />
         </StyledIconButton>
-        <StyledIconButton
-          className={cx(labeled && "MailListItem-labeled")}
-          classes={actionStyles}
-          onClick={() => setLabeled(!labeled)}
-        >
-          {labeled ? <Label /> : <LabelOutlined />}
+        <StyledIconButton classes={actionStyles}>
+          <LabelOutlined />
         </StyledIconButton>
       </div>
-      <Title>{title}</Title>
-      <Text>{description}</Text>
-      {hovered ? (
-        <Box flexShrink={0}>
-          <StyledTooltip title="Archive">
-            <GmailAction classes={actionStyles}>
-              <Archive />
-            </GmailAction>
-          </StyledTooltip>
-          <StyledTooltip title="Delete">
-            <GmailAction classes={actionStyles}>
-              <Delete />
-            </GmailAction>
-          </StyledTooltip>
-          <StyledTooltip title="Mark as read">
-            <GmailAction classes={actionStyles}>
-              <Drafts />
-            </GmailAction>
-          </StyledTooltip>
-          <StyledTooltip title="Snooze">
-            <GmailAction classes={actionStyles}>
-              <WatchLater />
-            </GmailAction>
-          </StyledTooltip>
-        </Box>
-      ) : (
-        <Date>
-          <b>May 21</b>
-        </Date>
-      )}
     </Div>
   );
 };
